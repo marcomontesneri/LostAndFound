5746e4a40bd2292e0c08dc3a2fc391f4bb3f061b
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2012-03-13 01:05:34 +0100

    loss declaration form now works
    added a modal
    error detection

--- static/script/scripts.js
+++ static/script/scripts.js
@@ -1,4 +1,25 @@
 (function(){
 	$('.datepicker').datepicker();
-	$('button').button();
+
+	var form = $('#declareLossForm'),
+		modal = $('#lossModal');
+
+	modal.modal({
+		show: false
+	});
+
+	form.submit(function(event){
+		event.preventDefault();
+		var $form = $(this),
+			lost = {
+				shortDesc: $form.find('input[name="lost[shortDesc]"]').val(),
+				where: $form.find('input[name="lost[where]"]').val(),
+				lostOn: $form.find('input[name="lost[lostOn]"]').val()
+			};
+		$.post('/api/lost', {lost:lost}, function(data){
+			modal.modal('show');
+			modal.find('.message').text(data.result == 'ok' ? "Hope we'll find it" : "Hum, something wrong just happened");
+		});
+		
+	});
 })();
