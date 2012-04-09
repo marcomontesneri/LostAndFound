5f8e14462d558847aff94755d1a2eeab65c0b8be
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2012-04-09 16:04:30 +0200

    removed temporary non related files

--- routes/Pages.js
+++ routes/Pages.js
@@ -15,11 +15,6 @@ module.exports = function(app, passport) {
 		});
 	});
 	
-
-	app.get('/perros', function(req, res) {
-		res.render('perros');
-	});
-	
 	app.get('/login', function(req, res) {
 		res.render('login', { 
 			user : req.user,
